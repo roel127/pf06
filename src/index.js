027c19c9ed826fe25890c74b715a525e2c4bfd1e
roel127 <yalleor127@gmail.com>
2023-09-17 20:55:44 +0900

    전역 데이터 연결

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Main from './Main';
+import store from './store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Main />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <Main />
+    </React.StrictMode>
+  </Provider>
 );
