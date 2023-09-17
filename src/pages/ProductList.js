25f601bb5d163412a7d29b17eeb52a0e27208891
roel127 <yalleor127@gmail.com>
2023-09-17 20:58:06 +0900

    컴포넌트화figure  업데이트

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -3,38 +3,47 @@ import $ from 'jquery';
 import { Link } from 'react-router-dom';
 import data from '../product.json';
 import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addToCart } from '../redux/User';
+import Item from '../components/Item';
 
-export function BrandForm( {brand, brandEx} ){
-  return(
-    <>
-    {
-      brandEx.length === 0 ? (<></>) : brandEx.map((item, index)=>{return(<p key={index}>{item}</p>)})
-    }
-    <div className="productList">
-      <span>전체 <b>{brand.length}</b>개의 제품이 있습니다.</span>
-      {brand.map(item=>{
-        return(
-          <figure key={item.id}>
-            <Link to={`/product/${item.slug}`}><img src={item.imgUrl} alt="" /></Link>
-            <figcaption>
-              <dl>
-                <dt><Link to={`/product/${item.slug}`}>{item.name}</Link></dt>
-                <dd>{item.base}</dd>
-                <dd>{item.price}</dd>
-                <dd>
-                  <button type="button">구매하기</button>
-                  <button type="button">장바구니</button>
-                </dd>
-              </dl>
-            </figcaption>
-          </figure>
-        )
-      })}
-    </div>
-    </>
+// export function BrandForm( {brand, brandEx} ){
+//   const { cartProductIds } = useSelector(state=>state.cart);
+//   const dispatch = useDispatch();
+//   console.log(cartProductIds);
 
-  )
-}
+//   return(
+//     <>
+//     {
+//       brandEx.length === 0 ? (<></>) : brandEx.map((item, index)=>{return(<p key={index}>{item}</p>)})
+//     }
+//     <div className="productList">
+//       <span>전체 <b>{brand.length}</b>개의 제품이 있습니다.</span>
+//       {brand.map(item=>{
+//         const str = item.slug.split('_')[0];
+//         return(
+//           <figure key={item.id}>
+//             <Link to={`/product/${item.slug}`}><img src={item.imgUrl} alt="" /></Link>
+//             <figcaption>
+//               <dl>
+//                 <dt><Link to={`/product/${item.slug}`}>{item.name}</Link></dt>
+//                 <dd>{item.base}</dd>
+//                 <dd>{item.price}</dd>
+//                 <dd>
+//                   <button type="button">구매하기</button>
+//                   <button type="button" onClick={()=>dispatch(addToCart({id:item.id, brand: str, count:1}))}>장바구니</button>
+//                 </dd>
+//               </dl>
+//             </figcaption>
+//           </figure>
+//         )
+//       })}
+
+//     </div>
+//     </>
+
+//   )
+// }
 
 export default function ProductList(){
   const allInit = [
@@ -57,6 +66,8 @@ export default function ProductList(){
     })
   }
 
+  const { cartProductIds } = useSelector(state=>state.cart);
+  const dispatch = useDispatch();
 
   return(
     <div id="productWrap">
@@ -66,7 +77,17 @@ export default function ProductList(){
           <button type="button" value='ramon' onClick={(e)=>changeBrand(e)}>Ramon<span></span></button>
           <button type="button" value='nishane' onClick={(e)=>changeBrand(e)}>Nishane<span></span></button>
       </p>
-      <BrandForm brand={brand} brandEx={brandEx} />
+      {/* <BrandForm brand={brand} brandEx={brandEx} /> */}
+      {
+        brandEx.length === 0 ? (<></>) : brandEx.map((item, index)=>{return(<p key={index}>{item}</p>)})
+      }
+      <div className="productList">
+        <span>전체 <b>{brand.length}</b>개의 제품이 있습니다.</span>
+        {brand.map(item=>{
+          return(<Item key={item.id} item={item}/>)
+        })}
+        
+      </div>
     </div>
   )
 }
