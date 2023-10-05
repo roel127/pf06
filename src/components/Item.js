7fe66f75a7a670d028ec319db4af69f1fa55d9d6
roel127 <yalleor127@gmail.com>
2023-10-05 15:26:50 +0900

    Refactor insert img's alt

--- src/components/Item.js
+++ src/components/Item.js
@@ -8,14 +8,14 @@ export default function Item( {item} ){
   const dispatch = useDispatch();
   return(
     <figure>
-      <Link to={item.slug}><img src={item.imgUrl} alt="" /></Link>
+      <Link to={item.slug}><img src={item.imgUrl} alt={item.name} /></Link>
       <figcaption>
         <dl>
           <dt><Link to={item.slug}>{item.name}</Link></dt>
           <dd>{item.base.join(', ')}</dd>
           <dd>{item.price}원</dd>
           <dd>
-            <button type="button">구매하기</button>
+            <button type="button">찜하기</button>
             <button onClick={ ()=> cartProductIds.filter(val=>val.id == item.id).length >= 1 ? alert('이미 장바구니에 있습니다.') : dispatch(addToCart({id:item.id, brand:item.slug.split('_')[0], count:1})) } type="button">장바구니</button>
           </dd>
         </dl>
