d2d832a4749d8283ebb7a7496247007e9bd1665a
roel127 <yalleor127@gmail.com>
2023-10-13 19:37:06 +0900

    Feat: 제품디테일 페이지 스크롤기능 구현 특정 지점에서 버튼 영역이 fixed 버튼 클릭 시 해당 영역으로 스크롤 class toggle에 관련해 class선택자의 css 추가

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -173,16 +173,36 @@ export function ProductReview( {cont} ){
 
 export default function Product(){
   const [countItem, setCountItem] = useState(1);
-  const params = useParams();
   const { cartProductIds } = useSelector(state=>state.cart);
+  const params = useParams();
   const dispatch = useDispatch();
-
   const str = params.slug.split('_');
   const cont = data[str[0]].stuff.filter(item=>{
     return item.id.length === 1 ? '0'+item.id == Number(str[1])-1 : item.id == Number(str[1])-1
   })[0];
-
   const totalPrice = Number(cont.price.replace(',','')) * countItem;
+
+  
+  window.addEventListener('scroll', function(){
+    if(this.window.scrollY > 1000){
+      document.querySelector('.bottomBtn').classList.add('fixed');
+    } else{
+      document.querySelector('.bottomBtn').classList.remove('fixed');
+    }
+  })
+  function scrollClick(e){
+    const text = e.target.textContent;
+    const itemInfo = document.querySelector('.bottomInfo').offsetTop;
+    const itemGuide = document.querySelector('.guide').offsetTop;
+    const itemReview = document.querySelector('.review').offsetTop;
+    if(text === '제품상세'){
+      window.scrollTo({top: itemInfo - 85, behavior: 'smooth'});
+    } else if(text === '상품구매안내'){
+      window.scrollTo({top: itemGuide - 85, behavior: 'smooth'});
+    } else{
+      window.scrollTo({top: itemReview - 85, behavior: 'smooth'});
+    }
+  }
   return(
     <div id='detail'>
       <section>
@@ -257,9 +277,9 @@ export default function Product(){
       </section>
       <section>
         <div className='bottomBtn'>
-          <button type='button'>제품상세</button>
-          <button type='button'>상품구매안내</button>
-          <button type='button'>리뷰 <span>&#40;0&#41;</span></button>
+          <button type='button' onClick={(e)=>scrollClick(e)}>제품상세</button>
+          <button type='button' onClick={(e)=>scrollClick(e)}>상품구매안내</button>
+          <button type='button' onClick={(e)=>scrollClick(e)}>리뷰 <span>&#40;0&#41;</span></button>
         </div>
         <div className='bottomInfo'>
           <ProductDetail cont={cont}/>
