3c766d06cf2b24e50a4be9f20e644405b4044b66
roel127 <yalleor127@gmail.com>
2023-10-19 19:44:28 +0900

    Test: 공지사항 구현 공지사항 제목을 클릭하면 해당 내용을 표시

--- src/pages/CommunityDetail.js
+++ src/pages/CommunityDetail.js
@@ -4,20 +4,30 @@ import noticeData from '../notice.json';
 
 export default function CommunityDetail(){
   const params = useParams();
-  const noticeId = params.slug;
-  const str = noticeData.notice[1].cont;
-  const strSpace = str.split('.');
-
+  const content = noticeData.notice;
+  let noticeNum;
+  noticeData.notice.forEach(notice=>{
+    const Ids = notice.id;
+    Ids.length <= 1 ? noticeNum = Number(params.slug.substring(7)) - 1 : noticeNum = Number(params.slug.substring(6)) - 1;
+  })
+  const str = content[noticeNum];
+  const strSpace = str.cont.split('.');
+  
+  // parmas.slug의 숫자 중에 앞에 0이 있다면 뒷 자리만 가지고 id를 추출하고 아니라면 숫자 전부를 가지고 id 추출
+  // 만약 세자리 숫자라면? 그럼 첫 번째 인덱스의 값이 0이 아닌데?? length별로 나눠야 한다...
+  const slugNum = params.slug.substring(6);
+  // slugNum[0] == 0 ? 
+  console.log(slugNum);
 
   return(
     <>
     <div className='board'>
       <div className='boardInfo'>
-      <p>8월 연휴 배송관련 공지</p>
+      <p>{str.subject}</p>
       <ul>
         <li>
-          <span>ssensefragrance</span>
-          <span>0000-00-00</span>
+          <span>{str.writer}</span>
+          <span>{str.reportDate}</span>
         </li>
         <li>조회수 <span>00</span></li>
       </ul>
